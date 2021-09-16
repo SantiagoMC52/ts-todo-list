e4f591078471818e196ff76c8ab4105870cb44b2
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-09-17 00:43:22 +0200

    Create add task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
+import { ITask } from './Interfaces';
 
-function App() {
+const App:FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [deadLine, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = {
+      taskName: task,
+      deadline: deadLine
+    };
+    setTodoList([...todoList, newTask]);
+    setTask('');
+    setDeadline(0);
+  };
+
   return (
-    <>
-      <h2>React todo list app with TypeScript</h2>
-    </>
+    <div>
+      <div>
+        <input type="text" placeholder="Task..." name="task" value={task} onChange={handleChange} />
+        <input type="number" placeholder="Deadlibe (in days...)" name="deadline" value={deadLine} onChange={handleChange} />
+        <button type="button" onClick={addTask}>Add task</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
